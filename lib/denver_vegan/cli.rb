1742e178d2c0daf02342c4ea3b60c59b72e815e2
osha7 <ogroetz@gmail.com>
2020-05-06 16:23:04 -0600

    updated cli

--- lib/denver_vegan/cli.rb
+++ lib/denver_vegan/cli.rb
@@ -1,6 +1,7 @@
 class DenverVegan::CLI
 
     def call
+        welcome
         DenverVegan::Scraper.scrape_cntraveler
         #binding.pry
         list_vegan_restaurants
@@ -8,39 +9,72 @@ class DenverVegan::CLI
         goodbye
     end
 
+    def welcome
+        puts ""
+        puts "Welcome!"
+        puts "We have compiled some of the best restaurants in Denver, where you can eat plants 'til your hearts content!"
+        puts ""
+        puts "Are You A Hungry Vegan In Denver?"    
+        input = gets.strip.downcase
+            if input == "y"
+            list_vegan_restaurants
+            elsif input == "n"
+                puts ""
+                puts "Must have had some real bad veg sides as a kid!"
+                exit
+            else
+                puts ""
+                puts "Just like meat, that's not an option."
+                welcome
+    end
+    end
+
+
     def list_vegan_restaurants
-        puts 
-        puts "Are You A Hungry Vegan In Denver?"       
-         
+
         @restaurants = DenverVegan::Restaurant.all
         @restaurants.each.with_index(1) do |rest, index|  
             puts "#{index}. #{rest.name}"
             #puts "#{index}. #{rest.name} - #{rest.price_point} - #{rest.review_snippet}"
         end
-     end
+    end
  
+    
      def menu
          
          input = nil
          while input != "exit"
-             puts "To choose the restaurant you'd like more info on:"
-             puts "Select number from above, type list to see the restaurants again or type exit:"
-             input = gets.strip
+            puts ""
+            puts "    **********************************************************"
+            puts "    *                  *                  *                  *"
+            puts "    * CHOOSE A NUMBER  *       LIST       *       EXIT       *"
+            puts "    *                  *                  *                  *"
+            puts "    **********************************************************"
+             puts "See a restaurant you'd like more info on? Select a number from above:"
+             puts "Type 'list' at anytime to see the restaurant list again."
+             puts "Typing 'exit' will get you outta here fast!"
+             input = gets.strip.downcase
 
-             if input.to_i > 0
-                the_restaurant = @restaurants[input.to_i-1]
-                puts "#{the_restaurant.name} - #{the_restaurant.price_point} - #{the_restaurant.review_snippet}"
-             elsif input == "list"
-                 list_vegan_restaurants  
-             else
-                 puts "Just like meat, that's not an option."
-             end
+                if input.to_i > 0
+                    the_restaurant = @restaurants[input.to_i-1]
+                    puts ""
+                    puts "#{the_restaurant.name} - #{the_restaurant.price_point} - #{the_restaurant.review_snippet}"
+                    
+                elsif input == "list"
+                    list_vegan_restaurants
+                elsif input == "exit"    
+                   puts "Thank you for eating your veggies!"
+                else 
+                    puts "Just like meat, that's not an option."
+                end
+
          end
  
      end
  
      def goodbye
-         puts "Thank you for eating your veggies!"
+        puts ""
+        puts "Now, go get some grub!"
      end
 
 end
